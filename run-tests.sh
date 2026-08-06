6ce4985810de4d2f08202ab765dd1acab1aef5b7
Pablo Tamarit <pablo.tamarit@cern.ch>
2026-08-06 15:37:22 +0200

    ci: fix isort CLI options for latest version

--- run-tests.sh
+++ run-tests.sh
@@ -9,7 +9,7 @@
 # TODO: Fix when patched in Flask-Admin (or remove dependency)
 #pipenv check && \
 pipenv run pydocstyle asclepias_broker tests docs && \
-pipenv run isort -rc -c -df && \
+pipenv run isort -c --df . && \
 pipenv run check-manifest --ignore "docs/_build*" && \
 #pipenv run sphinx-build -qnNW docs docs/_build/html && \
 pipenv run test
