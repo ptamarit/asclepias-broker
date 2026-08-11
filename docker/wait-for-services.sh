0c23812fc7e6c51f99a0e24de483e09cbd93d2b0
Pablo Tamarit <pablo.tamarit@cern.ch>
2026-08-11 16:09:39 +0200

    wait for services with exponential backoff

--- docker/wait-for-services.sh
+++ docker/wait-for-services.sh
@@ -10,10 +10,12 @@
 # Verify that all services are running before continuing
 check_ready() {
     RETRIES=5
+    EXP_BACKOFF_TIME=2
     while ! $2
     do
         echo "Waiting for $1, $((RETRIES--)) remaining attempts..."
-        sleep 2
+        sleep $EXP_BACKOFF_TIME
+        EXP_BACKOFF_TIME=$(( EXP_BACKOFF_TIME*2 ))
         if [ $RETRIES -eq 0 ]
         then
             echo "Couldn't reach $1"
@@ -21,14 +23,15 @@ check_ready() {
         fi
     done
 }
-_db_check(){ docker compose exec --user postgres db bash -c "pg_isready" &>/dev/null; }
-check_ready "postgres" _db_check
 
-_es_check(){ curl --output /dev/null --silent --head --fail http://localhost:9200 &>/dev/null; }
-check_ready "Elasticsearch" _es_check
+_db_check(){ docker compose exec --user postgres db bash -c "pg_isready" &>/dev/null; }
+check_ready "Postgres" _db_check
+
+_search_check(){ curl --output /dev/null --silent --head --fail http://localhost:9200 &>/dev/null; }
+check_ready "OpenSearch" _search_check
 
 _redis_check(){ docker compose exec cache bash -c 'redis-cli ping' | grep 'PONG' &> /dev/null; }
-check_ready "redis" _redis_check
+check_ready "Redis" _redis_check
 
 _rabbit_check(){ docker compose exec mq bash -c "rabbitmqctl status" &>/dev/null; }
 check_ready "RabbitMQ" _rabbit_check
