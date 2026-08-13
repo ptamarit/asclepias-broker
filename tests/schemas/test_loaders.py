99838734e29333a779220801c2488c90eae048f7
Pablo Tamarit <pablo.tamarit@cern.ch>
2026-08-13 19:08:34 +0200

    tests: fix marshmallow 3 not returning tuples on load anymore

--- tests/schemas/test_loaders.py
+++ tests/schemas/test_loaders.py
@@ -9,6 +9,7 @@
 
 import pytest
 from tests.helpers import gen_identifier, gen_relation
+from marshmallow.exceptions import ValidationError
 
 from asclepias_broker.core.models import Identifier, Relation, Relationship
 from asclepias_broker.schemas.loaders import IdentifierSchema, \
@@ -58,12 +59,14 @@ def rel_obj(source, relation, target):
 @pytest.mark.skip(reason="IDScheme validation temporarily disabled.")
 def test_identifier_schema(in_id, out_id, out_error, db, es_clear):
     """Test the schema for identifier."""
-    identifier, errors = IdentifierSchema().load(gen_identifier(*in_id))
-    if out_error:
-        assert errors == out_error
-    else:
-        assert not errors
+    try:
+        identifier = IdentifierSchema().load(gen_identifier(*in_id))
         compare_identifiers(identifier, id_obj(*out_id))
+    except ValidationError as errors:
+        if out_error:
+            assert errors == out_error
+        else:
+            raise
 
 
 @pytest.mark.parametrize(('in_rel', 'out_rel', 'out_error'), [
@@ -86,9 +89,11 @@ def test_identifier_schema(in_id, out_id, out_error, db, es_clear):
 ])
 def test_relationship_schema(in_rel, out_rel, out_error, db, es_clear):
     """Test the schema for relationship."""
-    relationship, errors = RelationshipSchema().load(rel_dict(*in_rel))
-    if out_error:
-        assert errors == out_error
-    else:
-        assert not errors
+    try:
+        relationship = RelationshipSchema().load(rel_dict(*in_rel))
         compare_relationships(relationship, rel_obj(*out_rel))
+    except ValidationError as errors:
+        if out_error:
+            assert errors == out_error
+        else:
+            raise
